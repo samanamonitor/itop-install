234aa76289c0873249418eb9e0d16078a4cf1167
Fabian Baena <fabian.baena@samanagroup.co>
2021-04-13 13:53:58 -0400

    migrate configuration to separate file

--- itop-asterisk/setqueuemembers.py
+++ itop-asterisk/setqueuemembers.py
@@ -1,8 +1,13 @@
 from astmanager import AstManager
 from itopmanager import iTopManager
 import config
+import sys
 
+if len(sys.argv) != 2:
+    print("Usage: %s <queue number>" % sys.argv[0])
+    exit(1)
 
+queue = argv[1]
 i = iTopManager(config.itop_user, config.itop_pw, 
     host=config.itop_host, port=config.itop_port)
 qmembers_sched=i.get_scheduled_members()
@@ -10,16 +15,16 @@ qmembers_sched
 
 a = AstManager(config.ast_username, config.ast_secret, host=config.ast_host, prefix=config.ast_prefix)
 a.login()
-current_members = a.get_queue_members(config.queue)
+current_members = a.get_queue_members(queue)
 
 add = []
 for i in qmembers_sched:
     if i not in current_members:
-        a.add_member(config.queue, "IAX2/%s" % i)
-        print("Adding %s from %s" % (i, config.queue))
+        a.add_member(queue, "IAX2/%s" % i)
+        print("Adding %s from %s" % (i, queue))
 
 remove = []              
 for i in current_members:
     if i not in qmembers_sched:
-        a.remove_member(config.queue, "IAX2/%s" % i)
-        print("Removing %s from %s" % (i, config.queue))
+        a.remove_member(queue, "IAX2/%s" % i)
+        print("Removing %s from %s" % (i, queue))
