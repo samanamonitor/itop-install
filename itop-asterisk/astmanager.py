d7cd16091226c1cfd8bdce5cee90aa23d75628de
Fabian Baena <fabian.baena@samanagroup.co>
2021-11-15 14:28:22 -0500

    remove double login

--- itop-asterisk/astmanager.py
+++ itop-asterisk/astmanager.py
@@ -15,7 +15,6 @@ class AstManager:
         self.port = port
         self.protocol = protocol
         self.prefix=prefix
-        self.login()
 
     def _request(self, data):
         qs = urlencode(data)
