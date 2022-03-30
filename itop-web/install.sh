159b888add70340b07f0706f52dab75593cd7116
Fabian Baena <fabian.baena@samanagroup.co>
2022-03-30 15:29:17 -0400

    change to volumes

--- itop-web/install.sh
+++ itop-web/install.sh
@@ -1,34 +1,56 @@
 #!/bin/bash
 
 SAMANA_PATH=/opt/samana-itop
-CONF_PATH=$SAMANA_PATH/conf
-LOG_PATH=$SAMANA_PATH/log
-BUILD_PATH=$SAMANA_PATH/build
-PROD_PATH=$SAMANA_PATH/prod
-EXTENSIONS_PATH=$SAMANA_PATH/extensions
+dirs="conf log prod build extensions"
 
-create_if_not_exist() {
+create_dir_if_not_exist() {
     local path=$1
     if [ ! -d "$path" ]; then
         mkdir -p $path
     fi
 }
 
+create_vol_if_not_exist() {
+    local vol_name=$1
+    local found
+    found=$(docker volume ls -q -f name=$vol_name)
+    if [ -z "$found" ]; then
+        docker volume create $vol_name
+    fi
+}
+
+create_lnk_if_not_exist() {
+    local vol=$1
+    local lnk=$2
+    if [ ! -l $lnk ]; then
+        ln -s $(docker inspect $vol | jq -r .[0].Mountpoint) $lnk
+    fi
+}
+
+create_bind() {
+    local dir=$1
+    create_vol_if_not_exist itop_$dir
+    create_lnk_if_not_exist itop_$dir $SAMANA_PATH/$dir
+}
+
+create_dir_if_not_exist $SAMANA_PATH
+
 IMAGE=$(docker image ls -q itop-web-img:v1)
 if [ -z "$IMAGE" ]; then
     docker build -t itop-web-img:v1 .
+    for d in $dirs; do
+        create_bind $d
+    done
+
+    chmod +rx /var/lib/docker/volumes
+    chmod +r /var/lib/docker/
 fi
 
-create_if_not_exist $CONF_PATH
-create_if_not_exist $LOG_PATH
-create_if_not_exist $BUILD_PATH
-create_if_not_exist $PROD_PATH
-create_if_not_exist $EXTENSIONS_PATH
 
 docker run -p 80:80 -p 443:443 -d  \
-    --mount type=bind,source="$CONF_PATH",target=/var/www/html/itop/conf/production \
-    --mount type=bind,source="$LOG_PATH",target=/var/log/apache2 \
-    --mount type=bind,source="$BUILD_PATH",target=/var/www/html/itop/env-production-build \
-    --mount type=bind,source="$PROD_PATH",target=/var/www/html/itop/env-production \
-    --mount type=bind,source="$EXTENSIONS_PATH",target=/var/www/html/itop/extensions \
+    --mount source=itop_conf,target=/var/www/html/itop/conf/production \
+    --mount source=itop_log,target=/var/log/apache2 \
+    --mount source=itop_build,target=/var/www/html/itop/env-production-build \
+    --mount source=itop_prod,target=/var/www/html/itop/env-production \
+    --mount source=itop_extensions,target=/var/www/html/itop/extensions \
     --name itop-web itop-web-img:v1 /usr/sbin/apache2ctl -DFOREGROUND
