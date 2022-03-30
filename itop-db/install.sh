1d2fedf7c05c8e03a02eaf3e70433acca47fef3f
Fabian Baena <fabian.baena@samanagroup.co>
2022-03-30 15:29:11 -0400

    change to volumes

--- itop-db/install.sh
+++ itop-db/install.sh
@@ -9,25 +9,52 @@ if [ -z "$DBPASS" ]; then
 fi
 
 SAMANA_PATH=/opt/samana-itop
-DB_PATH=$SAMANA_PATH/db
-DBLOG_PATH=$SAMANA_PATH/dblog
+dirs="db dblog"
 
-create_if_not_exist() {
+create_dir_if_not_exist() {
     local path=$1
     if [ ! -d "$path" ]; then
         mkdir -p $path
     fi
 }
 
+create_vol_if_not_exist() {
+    local vol_name=$1
+    local found
+    found=$(docker volume ls -q -f name=$vol_name)
+    if [ -z "$found" ]; then
+        docker volume create $vol_name
+    fi
+}
+
+create_lnk_if_not_exist() {
+    local vol=$1
+    local lnk=$2
+    if [ ! -l $lnk ]; then
+        ln -s $(docker inspect $vol | jq -r .[0].Mountpoint) $lnk
+    fi
+}
+
+create_bind() {
+    local dir=$1
+    create_vol_if_not_exist itop_$dir
+    create_lnk_if_not_exist itop_$dir $SAMANA_PATH/$dir
+}
+
+create_dir_if_not_exist $SAMANA_PATH
+
 IMAGE=$(docker image ls -q itop-db-img:v1)
 if [ -z "$IMAGE" ]; then
-    docker build -t itop-db-img:v1  . --build-arg DBPASS='$DBPASS'
+    docker build -t itop-db-img:v1 .
+    for d in $dirs; do
+        create_bind $d
+    done
+
+    chmod +rx /var/lib/docker/volumes
+    chmod +r /var/lib/docker/
 fi
 
-create_if_not_exist $DB_PATH
-create_if_not_exist $DBLOG_PATH
-
 docker run -p 3306:3306 -d \
-    --mount type=bind,source="$DB_PATH",target=/var/lib/mysql \
-    --mount type=bind,source="$DBLOG_PATH",target=/var/log/mysql \
+    --mount source=itop_db,target=/var/lib/mysql \
+    --mount source=itop_dblog,target=/var/log/mysql \
     --name itop-db itop-db-img:v1 mysqld_safe
