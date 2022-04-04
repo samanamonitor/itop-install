772ac17b34d66025a5ce6f7b82cac3f06bcffaa5
Fabian Baena <fabian.baena@samanagroup.co>
2022-04-04 18:19:03 -0400

    fix bug

--- samana-service-now/install.sh
+++ samana-service-now/install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-cp -R ../samana-service-now /opt/samana-itop/extensions
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+cp -R $DIR /opt/samana-itop/extensions
 chown -R www-data.www-data /opt/samana-itop/extensions/samana-service-now
 if [ -f /opt/samana-itop/conf/config-itop.php ]; then
     chmod u+w /opt/samana-itop/conf/config-itop.php
